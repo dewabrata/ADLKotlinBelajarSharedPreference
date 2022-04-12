b0f02394855fda9812752c32a13832a3311a6f9a
dewabrata <dewabrata@gmail.com>
2022-04-12 15:24:28 +0700

    Shared Preference remove key

--- app/src/main/java/com/adl/belajarsharedpreference/UjiLab.kt
+++ app/src/main/java/com/adl/belajarsharedpreference/UjiLab.kt
@@ -1,7 +1,10 @@
 package com.adl.belajarsharedpreference
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CompoundButton
 import android.widget.RadioGroup
 import android.widget.SeekBar
 import android.widget.SeekBar.OnSeekBarChangeListener
@@ -9,13 +12,20 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_uji_lab.*
 
 class UjiLab : AppCompatActivity() {
+
+     var sliderValue:Int = 0
+     var groupCombo:Int = 0
+    var switchComp:Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_uji_lab)
+        val sharedPreference = getSharedPreferences("uji_lab_setting", Context.MODE_PRIVATE)
 
         seekBar.setOnSeekBarChangeListener(object :SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
             txtSlider.setText("Nilainya adalah : ${p1.toString()}")
+                sliderValue = p1
 
             }
 
@@ -39,8 +49,44 @@ class UjiLab : AppCompatActivity() {
                    else -> "No Option"
                 }
                 Toast.makeText(this@UjiLab, "Pilihannya adalah ${resultt}",Toast.LENGTH_LONG).show()
+                groupCombo = p1
             }
 
         })
+
+        switch1.setOnCheckedChangeListener(object : CompoundButton.OnCheckedChangeListener{
+            override fun onCheckedChanged(p0: CompoundButton?, p1: Boolean) {
+                Toast.makeText(this@UjiLab,"nilainya adalah ${p1}",Toast.LENGTH_SHORT).show()
+                switch1.setText(p1.toString())
+                switchComp = p1
+            }
+        })
+
+        btnSave.setOnClickListener({
+
+            var editor = sharedPreference.edit()
+            editor.putInt("slider",sliderValue)
+            editor.putInt("combo",groupCombo)
+            editor.putBoolean("switch",switchComp)
+            editor.putString("text",txtInput.text.toString())
+            editor.commit()
+
+        })
+
+        loadSave(sharedPreference)
+
+    }
+
+    private fun loadSave(sharedPreference:SharedPreferences) {
+        sliderValue = sharedPreference.getInt("slider",0)
+        groupCombo = sharedPreference.getInt("combo",0)
+        switchComp = sharedPreference.getBoolean("switch",false)
+        txtInput.setText(sharedPreference.getString("text",""))
+
+        seekBar.setProgress(sliderValue)
+        radioGroup.check(groupCombo)
+        switch1.setChecked(switchComp)
+
+
     }
 }
